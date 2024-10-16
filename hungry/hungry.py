40aa984f35b1747be84b6a30d5511b9ec3c8d06e
AbdulazizAlghoraibi <abmg1500@gmail.com>
2024-10-17 01:49:11 +0300

    frise

--- hungry/hungry.py
+++ hungry/hungry.py
@@ -2,5 +2,7 @@ hungry=input("are you hungry? ")
 if hungry == "yes":
     print("eat some kabsa")
     print("eat some pizza")
+    print("eat some burger")
+    print("eat some frise")
 else:
     print("do your homework")
